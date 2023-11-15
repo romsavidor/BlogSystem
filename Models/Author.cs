2adde07c3980fa61246020369cc0a1433a115bbc
Rom Savidor <romsavidor@gmail.com>
2023-11-15 19:03:42 +0100

    Added in-memory DB created Author and Post classes

---
+++ Models/Author.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogSystem.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Author(int id, string name, string surname)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        Posts = new List<Post>();
+    }
+
+    public List<Post> Posts { get; set; }
+}
+
+
---
+++ Models/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogSystem.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public int AuthorId { get; set; }
+    public string Title {  get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+
+    public Post(int id,  int authorId, string title, string description, string content)
+    {
+        Id = id;
+        AuthorId = authorId;
+        Title = title;
+        Description = description;
+        Content = content;
+    }
+
+    public Author Author { get; set; }
+}
