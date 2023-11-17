220f632f746c002c58fce25c07397c70d253032c
Rom Savidor <romsavidor@gmail.com>
2023-11-17 16:45:34 +0100

    Added request DTO validations

--- Dtos/AuthorRequestDto.cs
+++ Dtos/AuthorRequestDto.cs
@@ -1,7 +1,12 @@
-﻿namespace BlogSystem.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogSystem.Dtos;
 
 public class AuthorRequestDto
 {
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
     public string Name { get; set; }
+
+    [MinLength(2, ErrorMessage = "Surname must be at least 2 characters")]
     public string Surname { get; set; }
 }
--- Dtos/PostRequestDto.cs
+++ Dtos/PostRequestDto.cs
@@ -1,10 +1,18 @@
-﻿namespace BlogSystem.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogSystem.Dtos;
 
 public class PostRequestDto
 {
-    // TODO: add validation rules?
     public int AuthorId { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
     public string Title { get; set; }
+
+    [Required]
     public string Description { get; set; }
+
+    [MinLength(100, ErrorMessage = "Content must be at least 100 characters")]
     public string Content { get; set; }
 }
