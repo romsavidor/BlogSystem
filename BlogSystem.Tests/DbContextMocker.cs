36d568b594af94ce5fc2f444060ffc570549cdff
Rom Savidor <romsavidor@gmail.com>
2023-11-22 03:58:00 +0100

    Created PostServiceTests, context mocker, Test seed data Moved git root folder

---
+++ BlogSystem.Tests/DbContextMocker.cs
@@ -0,0 +1,21 @@
+﻿using BlogSystem.Data;
+using BlogSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace BlogSystem.Tests;
+
+public static class DbContextMocker
+{
+    public static Mock<BlogDbContext> GetBlogDbContextMock()
+    {
+        var posts = new Mock<DbSet<Post>>();
+        var authors = new Mock<DbSet<Author>>();
+
+        var contextMock = new Mock<BlogDbContext>();
+        contextMock.Setup(c => c.Posts).Returns(posts.Object);
+        contextMock.Setup(c => c.Authors).Returns(authors.Object);
+
+        return contextMock;
+    }
+}
---
+++ BlogSystem.Tests/Helpers/TestSeedData.cs
@@ -0,0 +1,40 @@
+﻿using BlogSystem.Data;
+using BlogSystem.Models;
+
+namespace BlogSystem.Tests.Helpers;
+
+public class TestSeedData : IDisposable
+{
+    public Author SeededAuthor { get; private set; }
+    public Post SeededPost { get; private set; }
+
+    private readonly BlogDbContext _context;
+
+    public TestSeedData(BlogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedTestDataAsync(bool includeAuthor = true, bool includePost = true)
+    {
+        if (includeAuthor)
+        {
+            SeededAuthor = new Author("TestAuthor", "TestSurname");
+            _context.Authors.Add(SeededAuthor);
+        }
+
+        if (includePost)
+        {
+            SeededPost = new Post(1, "Test Title", "Test Description",
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m");
+            _context.Posts.Add(SeededPost);
+        }
+
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
